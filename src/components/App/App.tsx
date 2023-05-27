94a38d9d3c46f0bbe6ef0006a24f4807d62637c5
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2023-05-27 14:47:01 +0300

    добавить компонент random

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import { ContactsPage } from '../../pages/contacts/contacts';
 import { AroundPage } from '../../pages/around/around';
 import { Header } from '../Header/Header';
 import { FC } from 'react';
+import { RandomPage } from '../../pages/random/random';
 
 export const App: FC = () => {
   const location = useLocation();
@@ -19,6 +20,7 @@ export const App: FC = () => {
         </Route>
         <Route path='/contacts' element={<ContactsPage />} />
         <Route path='/map' element={<AroundPage />} />
+        <Route path='/random' element={<RandomPage />} />
       </Routes>
       <Header />
     </div>
---
+++ src/pages/random/random.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+import st from "./random.module.css";
+
+export const RandomPage = () => {
+ return (
+  <div>
+    <img className={st.img} src="https://stellarburgers.nomoreparties.site/static/media/loading.89540200.svg"></img>
+    <p className={st.p}>Функционал в разработке</p>
+  </div>
+ )
+}
